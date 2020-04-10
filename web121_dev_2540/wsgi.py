e4fbdd4053a8c0fd1628f6c443d125212b505120
Crowdbotics <team@crowdbotics.com>
2020-04-10 10:43:43 +0000

    Initial commit.

---
+++ web121_dev_2540/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for web121_dev_2540 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'web121_dev_2540.settings')
+
+application = get_wsgi_application()
